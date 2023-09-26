dfe1e4b6bf1902d8321c32327ec81262b47e456a
gaokefei <260480378@qq.com>
2023-09-26 11:55:01 +0800

    fix: type module error

--- tsup.config.ts
+++ tsup.config.ts
@@ -8,4 +8,13 @@ export default <Options>{
   format: ['cjs', 'esm'],
   dts: true,
   onSuccess: 'npm run build:fix',
+  banner:{
+    js: `
+// BANNER START
+const require = (await import("node:module")).createRequire(import.meta.url);
+const __filename = (await import("node:url")).fileURLToPath(import.meta.url);
+const __dirname = (await import("node:path")).dirname(__filename);
+// BANNER END
+    `
+  },
 }
